2cba03ba2f88a3bebe54e9daace10de870f3e00b
David Zamojda <zamojdadavid@gmail.com>
2019-02-25 16:37:18 -0500

    SQL database

---
+++ internships.sql
@@ -0,0 +1,28 @@
+
+CREATE DATABASE Internships;
+\c Internships
+
+
+CREATE TABLE Reviews (
+    Review_ID serial PRIMARY KEY,
+    company_name text NOT NULL,
+    job_title text NOT NULL,
+    salary int, 
+    start_date date,
+    end_date date,
+    rating int NOT NULL,
+    season text NOT NULL,
+    duration int NOT NULL,
+    address text NOT NULL,
+    street text default NOT NULL,
+    city text default NOT NULL,
+    state text default NOT NULL,
+    country text default NOT NULL,
+    Other_data text
+);
+
+CREATE TABLE Users (
+	user_name text PRIMARY KEY,
+	email text NOT NULL,
+);
+
