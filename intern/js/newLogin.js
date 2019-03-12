7a867ed61f8adad78f302b002d99252950891722
mkline3 <mdekline@gmail.com>
2019-03-12 15:06:20 -0400

    Update new user login, includes email varification

--- intern/js/newLogin.js
+++ intern/js/newLogin.js
@@ -1,3 +1,26 @@
-function sending(){
-	alert("Confirmation sent to: "+ $("#email").val());
-}
+
+let t = 0;
+
+$(document).ready(function(){
+	if(t===0){
+		$("#p1").hide();
+		$("#p2").hide();
+		$("#submit").hide()
+	}
+	$("#confirm").click(function(){
+		if($("#email").val().includes("@mail.umw.edu")
+			||  $("#email").val().includes("@umw.edu") ){
+			//alert("Confirmation sent to: "+ $("#email").val());
+			$("#p1").show();
+			$("#p2").show();
+			$("#submit").show();
+			$("#confirm").hide();
+			t = 1;
+		}
+		else{
+			alert("The entered email: "+ $("#email").val() + " is not a umw email");
+		}	
+
+	});
+});
+
