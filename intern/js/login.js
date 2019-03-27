af38d4a835ad99f24dd9f4dec6d03283c49c86b9
David Zamojda <zamojdadavid@gmail.com>
2019-03-26 21:59:51 -0400

    basic back end code

--- intern/js/login.js
+++ intern/js/login.js
@@ -1,13 +1,17 @@
 const express = require('express');
-var dateFormat = require('dateformat');
 var Pool = require('pg').Pool;
 var bodyParser = require('body-parser');
 
 const app = express();
+
+//create role on database
+//has select privleges
+
+
 var config = {
 	host: 'localhost',
-	user: 'person2',
-	password: 'RqqZ(@\+Eb\GW?9P',
+	user: 'postgres',
+	password: 'password',
 	database: 'internships',
 };
 
@@ -15,4 +19,20 @@ var pool = new Pool(config);
 app.set('port', (8080));
 app.use(bodyParser.json({type: 'application/json'}));
 app.use(bodyParser.urlencoded({extended: true}));
+
+app.get('/test', async (req, res) =>{
+	try{
+		var response = await pool.query('select * from reviews');
+		res.json(response.rows);
+	}
+	catch(e){
+			console.log('Error running get',e);
+	}
+
+})
+
+
+app.listen(app.get('port'), () => {
+	console.log('Running');
+})
 
