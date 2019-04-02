822492922660f5bdacbe09d504e58a1ae8b1e141
mkline3 <mdekline@gmail.com>
2019-04-02 11:17:34 -0400

    Tryying something

--- intern/js/review.js
+++ intern/js/review.js
@@ -5,6 +5,14 @@ $(document).ready(function(){
 	$.ajax({
 		url: 'http://localhost:8080/review',
 		type: "GET",
+		data:{
+			name: $("#name").val(),
+			state: $("#state").val(),
+			city: $("#city").val(),
+			pay: $("#pay").val(),
+			order:$("#rate").val(),
+
+		},
 		success: function(result){
 
 			console.log(result);
@@ -19,3 +27,4 @@ $(document).ready(function(){
 
 
 });
+//?name=#1&state=#2&city=#3&pay=#4&order=#5
